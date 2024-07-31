193cd0bf8148104979ed403814e4c6eb25a5f5ef
KhangAnh <taicuoi664@gmail.com>
2024-07-31 17:01:16 +0700

    add Phan02_Bai01.2_S(n)=1/2 + 2/3 + ... + n/n+1

--- Code_Tuan04/Phan02_Bai01.cpp
+++ Code_Tuan04/Phan02_Bai01.cpp
@@ -10,12 +10,23 @@ int sum(int n)
 	else return n + sum(n - 1);
 }
 
+float sum_2(int n)
+{
+	if (n==1)
+	{
+		return 0.5;
+	}
+	else return (float) n / (n + 1) + sum_2(n-1);
+}
+
 int main()
 {
 	int n;
 	printf("Nhap n:");
 	scanf("%d", &n);
 	printf("Ket Qua cua S(n)=1+2+3+...+n la: %d", sum(n));
+	printf("\n----------------------------------\n");
+	printf("Ket Qua cua S(n)=1/2 + 2/3 + ... + n/n+1 la: %.2f", sum_2(3));
 	getch();
 	return 0;
 }
