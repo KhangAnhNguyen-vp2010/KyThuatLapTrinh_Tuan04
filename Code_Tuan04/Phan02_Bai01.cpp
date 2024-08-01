9a051741273750856861467774ae324501df2695
Anh Khang <taicuoi664@gmail.com>
2024-08-01 13:12:14 +0700

    add Phan02_Bai01.7_S(n)= 1+sqrt(1+2)/2+sqrt(3GiaiThua) + 2+sqrt(2+3)/3+sqrt(4GiaiThua) + 3+sqrt(3+4)/4+sqrt(5GiaiThua) + ... + n+sqrt(n+n+1)/(n+1)+sqrt((n+2)GiaiThua)

--- Code_Tuan04/Phan02_Bai01.cpp
+++ Code_Tuan04/Phan02_Bai01.cpp
@@ -64,6 +64,15 @@ double sum_6(int n)
 	else return (double) ( n*giaiThua(n+1)/(n+1) + sqrtf(n+2) ) + sum_6(n-1);
 }
 
+double sum_7(int n)
+{
+	if (n==1)
+	{
+		return 1+sqrtf(3) / 2+sqrtf(giaiThua(3));
+	}
+	else return (double) n+sqrtf(n+n+1) / (n+1) + sqrtf(giaiThua(n+2)) + sum_7(n-1);
+}
+
 int main()
 {
 	int n;
@@ -80,6 +89,8 @@ int main()
 	printf("Ket Qua cua S(n)= 1.2 + 2.3 + 3.4 + 4.5 + ... + n.(n+1) la: %d", sum_5(n));
 	printf("\n----------------------------------\n");
 	printf("Ket Qua cua S(n)= 1.2!/2+sqrt(3) + 2.3!/3+sqrt(4) + 3.4!/4+sqrt(5) + ... + n.(n+1)!/(n+1)+sqrt(n+2) la: %.2f", sum_6(n));
-	getch();
+	printf("\n----------------------------------\n");
+	printf("Ket Qua cua S(n)= 1+sqrt(1+2)/2+sqrt(3!) + 2+sqrt(2+3)/3+sqrt(4!) + 3+sqrt(3+4)/4+sqrt(5!) + ... + n+sqrt(n+n+1)/(n+1)+sqrt((n+2)!) la: %.2f", sum_7(n));
+	getch(); 
 	return 0;
 }
