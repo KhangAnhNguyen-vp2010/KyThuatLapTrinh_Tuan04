49f26afc45eb02b7fd8c07996982fc330c4224f1
Anh Khang <taicuoi664@gmail.com>
2024-08-01 12:42:27 +0700

    add Phan02_Bai01.4_S(n)= 1 + 1/3 + 1/5 + ... + 1/2n+1

--- Code_Tuan04/Phan02_Bai01.cpp
+++ Code_Tuan04/Phan02_Bai01.cpp
@@ -28,6 +28,15 @@ double sum_3(int n)
 	else return sqrtf(5 + sum_3(n-1));
 }
 
+double sum_4(int n)
+{
+	if (n==0)
+	{
+		return 1;
+	}
+	else return (double) 1/(2*n+1) + sum_4(n-1); 
+}
+
 int main()
 {
 	int n;
@@ -38,6 +47,8 @@ int main()
 	printf("Ket Qua cua S(n)=1/2 + 2/3 + ... + n/n+1 la: %.2f", sum_2(3));
 	printf("\n----------------------------------\n");
 	printf("Ket Qua cua S(n)= sqrt(5 + sqrt(5 + ...  + sqrt(5 + sqrt(5)))) la: %.2f", sum_3(n));
+	printf("\n----------------------------------\n");
+	printf("Ket Qua cua S(n)= 1 + 1/3 + 1/5 + ... + 1/2n+1 la: %.2f", sum_4(n));
 	getch();
 	return 0;
 }
