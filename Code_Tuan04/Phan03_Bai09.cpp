043a8067ce1a0ba5ec53ff02c83c41d0e72cb2d7
Anh Khang <taicuoi664@gmail.com>
2024-08-01 18:18:39 +0700

    add Phan03_Bai09.3_S(n)=1+(1+2) + (1+2+3) + ... + (1+2+3+...+n)

--- Code_Tuan04/Phan03_Bai09.cpp
+++ Code_Tuan04/Phan03_Bai09.cpp
@@ -20,6 +20,24 @@ int Sum_2(int n)
 	else return powf(n,2)+Sum_2(n-1);
 }
 
+int tinhTong(int a)
+{
+	if (a==1)
+	{
+		return 1;
+	}
+	else return a+tinhTong(a-1);
+}
+
+int Sum_3(int n)
+{
+	if (n==1)
+	{
+		return 1;
+	}
+	else return tinhTong(n) + Sum_3(n-1);
+}
+
 int main()
 {
 	int n;
@@ -28,6 +46,8 @@ int main()
 	printf("Ket qua cua S(n)=1/1.2.3 + 1/2.3.4 + 1/3.4.5 + ... + 1/n.(n+1).(n+2) la: %.2f", Sum_1(n));
 	printf("\n------------------------------------\n");
 	printf("Ket qua cua S(n)= 1^2 + 2^2 + ... + n^2 la: %d", Sum_2(n));
+	printf("\n------------------------------------\n");
+	printf("Ket qua cua S(n)=1+(1+2) + (1+2+3) + ... + (1+2+3+...+n) la: %d", Sum_3(n));
 	getch();
 	return 0;
 }
